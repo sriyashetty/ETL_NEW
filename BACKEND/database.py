95acef803a8f367bf0d43d7165189155712c1260
yajnya <yajnya45@gmail.com>
2026-01-08 11:35:17 +0530

    database link

--- BACKEND/database.py
+++ BACKEND/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
  
 DATABASE_URL = (
-    "mssql+pyodbc://localhost/ETLSAMPLE"
+    "mssql+pyodbc://@DESKTOP-N3UHV52\\SQLEXPRESS/ETLSAMPLE"
     "?driver=ODBC+Driver+17+for+SQL+Server"
     "&trusted_connection=yes"
 )
